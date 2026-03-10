411777c9c0d34509cd1f1caac3ab11a9a7bb99b0
paasapcom <paa@sap.com>
2026-03-10 13:47:47 +0000

    site build ➝ gh pages

---
+++ @preview/delsoup455-innocent-docs-fix/assets/js/acecf23e.a5f86fed.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/@preview/delsoup455-innocent-docs-fix/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/delsoup455-innocent-docs-fix/blog/authors"}')}}]);
